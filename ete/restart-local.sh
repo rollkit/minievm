1d7260d4adc17a87c190e0f091cbc9bbbef84da2
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2024-05-17 22:47:17 +0200

    ete

---
+++ ete/restart-local.sh
@@ -0,0 +1,3 @@
+DA_BLOCK_HEIGHT=15
+AUTH_TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJwdWJsaWMiLCJyZWFkIiwid3JpdGUiLCJhZG1pbiJdfQ.fkjpl_WoUPvti3oKE6_523VSo0TTqASwRpr5lFTqvvY
+minitiad start --rollkit.aggregator --rollkit.aggregator --rollkit.da_auth_token=$AUTH_TOKEN --rollkit.da_namespace 00000000000000000000000000000000000000000008e5f679bf7116cb --rollkit.da_start_height $DA_BLOCK_HEIGHT --rpc.laddr tcp://127.0.0.1:36657 --grpc.address 127.0.0.1:9290 --p2p.laddr "0.0.0.0:36656" --minimum-gas-prices=0.025stake
